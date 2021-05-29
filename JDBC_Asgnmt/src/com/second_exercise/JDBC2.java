80ebe5888221904af215d2d5afee2b19b0be39e5
Bhargav <udutha.bhargav@hotmail.com>
2021-05-29 07:03:35 +0530

    Update JDBC2.java

--- JDBC_Asgnmt/src/com/second_exercise/JDBC2.java
+++ JDBC_Asgnmt/src/com/second_exercise/JDBC2.java
@@ -7,23 +7,42 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class JDBC2 {
-
-	public static void main(String[] args) throws Exception {
-		try
-		{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_asgnmt", "root", "root");
-		con.setAutoCommit(false);
-		Statement st=con.createStatement();
-		st.executeUpdate("insert into customers values('Bhargav',10000,'bhargav@hcl.com')");
-		con.commit();
-		}
-		catch(SQLException e)
-		{
-			con.rollback();
-			System.out.println("Email already exist");
-		}
-	}
+public static void main(String[] args) throws Exception {
+Connection con = null;
+try {
+while (true) {
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter name:");
+String name = sc.next();
+System.out.println("Enter Salary:");
+int salary = sc.nextInt();
+System.out.println("Enter email:");
+String email = sc.next();
+Class.forName("com.mysql.cj.jdbc.Driver");
+con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_asgnmt", "root", "root");
+con.setAutoCommit(false);
+PreparedStatement ps = con.prepareStatement("insert into customers values(?,?,?)");
+ps.setString(1, name);
+ps.setInt(2, salary);
+ps.setString(3, email);
+ps.executeUpdate();
+con.commit();
+System.out.println("Do you Want to add more records y/n");
+String ans = sc.next();
+if (!"y".equalsIgnoreCase(ans)) {
+System.out.println("Data saved successfully");
+break;
 }
+}
+} 
+catch (Exception e) {
+con.rollback();
+System.out.println("Email Id must be unique . Data Rollback successfully");
+} finally {
+con.close();
+}
+}
+}
+
 	
 
